624a04a5b9c431cd5e65ab3e0c37552d497aed28
Mintu sharma <ms5949625@gmail.com>
2023-05-25 16:56:14 +0530

    first commit

---
+++ app/src/main/java/com/mintusharma/movieapp/models/MovieList.kt
@@ -0,0 +1,7 @@
+package com.mintusharma.movieapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(@SerializedName("Search") var Search       : ArrayList<Search> = arrayListOf(),
+                     @SerializedName("totalResults") var totalResults : String?           = null,
+                     @SerializedName("Response") var Response     : String?           = null)
